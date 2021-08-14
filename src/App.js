a188abcdb04d073260e96a155ccce73f14f0a417
sedrak kocharyan <sedrakqocharyan@gmail.com>
2021-08-14 16:18:19 +0400

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { GoogleLogin } from "react-google-login";
+import React from "react";
 
-function App() {
+export default function App() {
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GoogleLogin
+        clientId="946913282394-ei7daebpskqmv0rea1aimduefisbnoje.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
+      ,
     </div>
   );
 }
-
-export default App;
